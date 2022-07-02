715174bbb1436858cb03b12176a4313e5803af80
Белов Артём <artem.belov.0320@gmail.com>
2022-07-02 17:06:24 +0500

    feat(home): Add characters table

--- src/API.js
+++ src/API.js
@@ -1,8 +1,38 @@
 const API_URL = 'https://brooms.herokuapp.com';
 
 export const API = {
-	ideas: async () => {
-		const data = await (await fetch(`${API_URL}/ideas`)).json()
-    return data._embedded.ideas;
-  },
+	characters: async () => {
+		return await [
+			{
+				id: '1',
+				nickname: 'Тестовый персонаж',
+				about: 'Тестовый персонаж без всякой предыстории и свойств)))',
+				avatar: 'https://picsum.photos/200',
+				registerDate: new Date('22-12-2001'),
+				race: 'Орк какой-то',
+				class: 'Убийца',
+				guild: 'Нечистая сила'
+			},
+			{
+				id: '2',
+				nickname: 'Какой-то her0',
+				about: 'ее персонаж опять же без свойств',
+				avatar: 'https://picsum.photos/200',
+				registerDate: new Date('01-01-2022'),
+				race: 'Эльф',
+				class: 'Лучник',
+				guild: 'Парикмахерская'
+			},
+			{
+				id: '3',
+				nickname: 'Бильбо',
+				about: 'Отправился воровать камень у дракона',
+				avatar: 'https://picsum.photos/200',
+				registerDate: new Date('02-02-2018'),
+				race: 'Хоббит',
+				class: 'Вор',
+				guild: 'Садоводы'
+			},
+		]
+	},
 };
