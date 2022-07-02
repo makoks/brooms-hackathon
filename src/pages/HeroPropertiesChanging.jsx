caa8a38366d3bb07e6f14eb56d0b6ca0a79c1bc6
Белов Артём <artem.belov.0320@gmail.com>
2022-07-03 04:37:11 +0500

    fix(hero): Add static info

--- src/pages/HeroPropertiesChanging.jsx
+++ src/pages/HeroPropertiesChanging.jsx
@@ -4,12 +4,12 @@ import {ContentHeader} from '../components';
 import {useParams} from "react-router-dom";
 import {API} from "../API";
 import HeroMainInfo from "../components/Characters/CharacterPage/HeroMainInfo";
-import ClustersList from "../components/Characters/CharacterPage/ClustersList/ClustersList";
 
 const {Option} = Select
 
 export const HeroPropertiesChanging = () => {
 	const {id} = useParams()
+	const [loading, setLoading] = useState(false)
 	const [character, setCharacter] = useState(undefined)
 	const [editableClusters, setEditableClusters] = useState(undefined)
 	const [reason, setReason] = useState('levelUp')
@@ -28,21 +28,21 @@ export const HeroPropertiesChanging = () => {
 		toggleIsEdit()
 	}
 
-	const changeClusterField = (clusterName, fieldName, newValue) => {
-		const newClusters = editableClusters.map(cluster => (
-			cluster.name === clusterName
-				? {
-					...cluster,
-					items: cluster.items.map(item => (
-						item.name === fieldName
-							? {...item, value: newValue}
-							: item
-					))
-				}
-				: cluster
-		))
-		setEditableClusters(newClusters)
-	}
+	// const changeClusterField = (clusterName, fieldName, newValue) => {
+	// 	const newClusters = editableClusters.map(cluster => (
+	// 		cluster.name === clusterName
+	// 			? {
+	// 				...cluster,
+	// 				items: cluster.items.map(item => (
+	// 					item.name === fieldName
+	// 						? {...item, value: newValue}
+	// 						: item
+	// 				))
+	// 			}
+	// 			: cluster
+	// 	))
+	// 	setEditableClusters(newClusters)
+	// }
 
 	const toggleIsEdit = () => {
 		setIsEdit(!isEdit)
@@ -54,21 +54,23 @@ export const HeroPropertiesChanging = () => {
 			setCharacter(response.data)
 			setEditableClusters(response.data.clusters)
 		}
-
+		setLoading(true)
 		getCharacter(id)
+			.finally(() => setLoading(false))
 	}, [id])
 
 	return (
 		<Layout>
 			<ContentHeader title='Персонаж' paddingBottom={true}>
 				<HeroMainInfo
+					loading={loading}
 					avatar={character?.avatarHero}
 					nickname={character?.heroName}
 					about={character?.about}
 					registerDate={new Date(character?.dateReg)}
-					race={character?.race}
-					heroClass={character?.heroClass}
-					guild={character?.guild}
+					race={character?._embedded.heroRace.raceName}
+					heroClass={character?._embedded.heroClass.nameClass}
+					guild={character?._embedded.heroGuild.nameGuild}
 				/>
 				<Space>
 					Редактировать:
@@ -97,12 +99,12 @@ export const HeroPropertiesChanging = () => {
 						</Select>
 					</div>
 				)}
-				<ClustersList
-					clusters={character?.clusters ?? []}
-					editableClusters={editableClusters}
-					changeClusterField={changeClusterField}
-					isEdit={isEdit}
-				/>
+				{/*<ClustersList*/}
+				{/*	clusters={character?.clusters ?? []}*/}
+				{/*	editableClusters={editableClusters}*/}
+				{/*	changeClusterField={changeClusterField}*/}
+				{/*	isEdit={isEdit}*/}
+				{/*/>*/}
 				{isEdit && (
 					<Space style={{marginTop: 16, marginLeft: 'auto'}}>
 						<Button onClick={saveChanges} type='primary'>Сохранить изменения</Button>
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,13 +1,80 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {Layout, Select, TreeSelect} from 'antd';
 import {ContentHeader} from '../components';
+
+const mutableProperties = [
+	{
+		title: 'Cluster 1',
+		value: '0-0',
+		key: '0-0',
+		children: [
+			{
+				title: 'MP 1',
+				value: '0-0-0',
+				key: '0-0-0',
+			},
+			{
+				title: 'MP 2',
+				value: '0-0-1',
+				key: '0-0-1',
+			},
+		],
+	},
+	{
+		title: 'Cluster 2',
+		value: '0-1',
+		key: '0-1',
+		children: [
+			{
+				title: 'MP 3',
+				value: '0-1-0',
+				key: '0-1-0',
+			},
+			{
+				title: 'MP 4',
+				value: '0-1-1',
+				key: '0-1-1',
+			},
+			{
+				title: 'MP 5',
+				value: '0-1-2',
+				key: '0-1-2',
+			},
+		],
+	},
+];
 
 export const History = () => {
 	return (
 		<Layout>
-			<ContentHeader title='History' paddingBottom={true}/>
+			<ContentHeader title='История изменений' paddingBottom={true}>
+				<Select
+					showSearch
+					placeholder="Select a hero"
+					optionFilterProp="children"
+					onChange={undefined}
+					filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+					mode="multiple"
+					style={{ width: '100%' }}
+					allowClear={true}
+					maxTagCount={6}
+					showArrow={true}
+				>
+					<Select.Option value="jack">Hero 1</Select.Option>
+					<Select.Option value="lucy">Hero 2</Select.Option>
+					<Select.Option value="tom">Hero 3</Select.Option>
+				</Select>
+				<TreeSelect
+					treeData={mutableProperties}
+					treeCheckable={true}
+					style={{ width: '100%' }}
+					placeholder="Select properties"
+					allowClear={true}
+					showArrow={true}
+					treeDefaultExpandAll={true}
+				/>
+			</ContentHeader>
 			<Layout.Content style={{margin: '27px 34px'}}>
-
 			</Layout.Content>
 		</Layout>
 	);
