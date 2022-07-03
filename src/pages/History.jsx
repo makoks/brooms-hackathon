70d164b4166e405f7ce70c6c4bc1917fec5003d4
Белов Артём <artem.belov.0320@gmail.com>
2022-07-03 07:34:10 +0500

    feat(history): Add history report

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,80 +1,109 @@
-import React from 'react';
-import {Layout, Select, TreeSelect} from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Layout, Select, Space, Typography, DatePicker, Button, Table} from 'antd';
 import {ContentHeader} from '../components';
+import {API} from "../API";
 
-const mutableProperties = [
+const {RangePicker} = DatePicker
+const {Option} = Select
+const {Column, ColumnGroup} = Table
+
+const data = [
 	{
-		title: 'Cluster 1',
-		value: '0-0',
-		key: '0-0',
-		children: [
-			{
-				title: 'MP 1',
-				value: '0-0-0',
-				key: '0-0-0',
-			},
-			{
-				title: 'MP 2',
-				value: '0-0-1',
-				key: '0-0-1',
-			},
-		],
+		key: '1',
+		changeDate: new Date('12-12-2000').toLocaleDateString('ru-RU'),
+		reason: 'Повышение уровня',
+		stats: {
+			level: 32,
+			mana: 1212,
+			gold: 1212121,
+		},
+		loot: {
+			hat: 'Железо',
+			boots: 'Кожа',
+			armor: 'Лед'
+		}
 	},
 	{
-		title: 'Cluster 2',
-		value: '0-1',
-		key: '0-1',
-		children: [
-			{
-				title: 'MP 3',
-				value: '0-1-0',
-				key: '0-1-0',
-			},
-			{
-				title: 'MP 4',
-				value: '0-1-1',
-				key: '0-1-1',
-			},
-			{
-				title: 'MP 5',
-				value: '0-1-2',
-				key: '0-1-2',
-			},
-		],
+		key: '2',
+		changeDate: new Date('01-11-1978').toLocaleDateString('ru-RU'),
+		reason: 'Повышение уровня',
+		stats: {
+			level: 54,
+			mana: 181,
+			gold: 12120,
+		},
+		loot: {
+			hat: 'Ткань',
+			boots: 'Кожа',
+			armor: 'Железо'
+		}
+	},
+	{
+		key: '3',
+		changeDate: new Date('10-10-1998').toLocaleDateString('ru-RU'),
+		reason: 'Выход из игры',
+		stats: {
+			level: 50,
+			mana: 111,
+			gold: 12000,
+		},
+		loot: {
+			hat: 'Огонь',
+			boots: null,
+			armor: 'Дерево'
+		}
 	},
 ];
 
 export const History = () => {
+	const [dates, setDates] = useState([])
+	const [hero, setHero] = useState(undefined)
+	const [heroes, setHeroes] = useState([])
+
+	useEffect(() => {
+		const getHeroes = async () => {
+			const res = await API.getHeroes()
+			setHeroes(res.data._embedded.hero)
+		}
+
+		getHeroes()
+	}, [])
+
 	return (
 		<Layout>
 			<ContentHeader title='История изменений' paddingBottom={true}>
 				<Select
 					showSearch
-					placeholder="Select a hero"
-					optionFilterProp="children"
-					onChange={undefined}
 					filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-					mode="multiple"
-					style={{ width: '100%' }}
-					allowClear={true}
-					maxTagCount={6}
-					showArrow={true}
+					onChange={setHero}
+					value={hero}
+					style={{width: '100%'}}
 				>
-					<Select.Option value="jack">Hero 1</Select.Option>
-					<Select.Option value="lucy">Hero 2</Select.Option>
-					<Select.Option value="tom">Hero 3</Select.Option>
+					{heroes.map(hero => (
+						<Option value={hero.id} key={hero.id}>{hero.heroName}</Option>
+					))}
 				</Select>
-				<TreeSelect
-					treeData={mutableProperties}
-					treeCheckable={true}
-					style={{ width: '100%' }}
-					placeholder="Select properties"
-					allowClear={true}
-					showArrow={true}
-					treeDefaultExpandAll={true}
-				/>
+				<Space>
+					<Typography.Text>Выберите период изменений</Typography.Text>
+					<RangePicker value={dates} onCalendarChange={setDates}/>
+					<Button type='primary'>Применить</Button>
+				</Space>
 			</ContentHeader>
 			<Layout.Content style={{margin: '27px 34px'}}>
+				<Table dataSource={data} bordered>
+					<Column title="Дата изменения" dataIndex="changeDate" key="changeDate" />
+					<Column title="Причина" dataIndex="reason" key="reason" />
+					<ColumnGroup title='Статистика'>
+						<Column title="Уровень" dataIndex={['stats', 'level']} key='stats.level' />
+						<Column title="Мана" dataIndex={['stats', 'mana']} key="stats.mana" />
+						<Column title="Золото" dataIndex={['stats', 'gold']} key="stats.gold" />
+					</ColumnGroup>
+					<ColumnGroup title='Лут'>
+						<Column title="Шляпа" dataIndex={['loot', 'hat']} key='loot.hat' />
+						<Column title="Ботинки" dataIndex={['loot', 'boots']} key="loot.boots" />
+						<Column title="Броня" dataIndex={['loot', 'armor']} key="loot.armor" />
+					</ColumnGroup>
+				</Table>
 			</Layout.Content>
 		</Layout>
 	);
