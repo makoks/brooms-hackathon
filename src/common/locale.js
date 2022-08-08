6ac9b484efe64f1eb675f9f01b76ef11ef319bc1
Белов Артём <artem.belov.0320@gmail.com>
2022-08-08 16:04:47 +0500

    feat(home): Добавлена фильтрация и сортировка в таблицу сотрудников

---
+++ src/common/locale.js
@@ -0,0 +1,9 @@
+export const tableLocale = {
+	filterSearchPlaceholder: 'Фильтр',
+	emptyText: 'Нет данных',
+	filterReset: 'Сбросить',
+	filterEmptyText: 'Нет фильтров',
+	triggerDesc: 'Нажмите для сортировки "Я-А"',
+	triggerAsc: 'Нажмите для сортировки "А-Я"',
+	cancelSort: 'Нажмите для отмены сортировки'
+}
