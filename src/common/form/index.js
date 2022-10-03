8bd63431078eaa2c971be0334f00c42a2c925d1e
Белов Артём <artem.belov.0320@gmail.com>
2022-10-03 12:19:54 +0500

    fix(project): bugs

--- src/common/form/index.js
+++ src/common/form/index.js
@@ -8,7 +8,12 @@ export const emailRules = {
 	message: 'Почта введена некорректно'
 }
 
+export const maxLengthRule = (len) => ({
+	max: len,
+	message: 'Превышена максимальная длина'
+})
+
 // export const phoneNumberRules = {
-// 	pattern: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
+// 	pattern: /^((\d)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
 // 	message: 'Номер телефона введен некорректно'
 // }
